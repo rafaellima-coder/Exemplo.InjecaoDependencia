d2aa6d42caa5bfb87d40cf96011dbc0a8a5d4ab8
RafaelPerez de Lima <dr.rafalima@hotmail.com>
2020-09-13 13:09:47 -0300

    Add project files.

---
+++ ContraExemplo/Endereco.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContraExemplo
+{
+    public class Endereco
+    {
+        private string _logradouro;
+        private int _numero;
+        public string Logradouro
+        {
+            get { return _logradouro; }
+            set { _logradouro = value; }
+        }
+
+        public int Numero
+        {
+            get { return _numero; }
+            set { _numero = value; }
+        }
+
+
+    }
+}
